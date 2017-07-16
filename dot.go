f7bf7f9110b1b7cf386df050fb28c7ace90e9574
Andreas Pannewitz <GoLangsam.APA@a-bc.net>
2017-07-16 17:06:07 +0200

    add: dot.go

---
+++ dot.go
@@ -0,0 +1,8 @@
+﻿// Copyright 2017 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// directory internal
+package internal
+
+const dot = `.` // just a noop - avoids 'no buildable Go source files' warning of `go get`
